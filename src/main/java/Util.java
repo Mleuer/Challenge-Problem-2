969a0020db5938aa6b3bed867ad6bca69da4907f
Matt Leuer <mattleuer@gmail.com>
2019-07-30 16:30:56 -0700

    Implemented calculateMedianIncome. AlphabetizeByLastName is WIP.

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,4 +1,7 @@
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Util {
@@ -44,9 +47,11 @@ public class Util {
                 people) {
             if (person.highestLevelOfEducation == "High School") {
                 highSchool++;
-            }else if (person.highestLevelOfEducation == "College") {
+            }
+            if (person.highestLevelOfEducation == "College") {
                 college++;
-            }else other++;
+            }
+            else other++;
         }
         if (college > highSchool) {
             return collegeString;
@@ -55,4 +60,34 @@ public class Util {
         }else return otherString;
     }
 
+    public static int calculateMedianIncome(List<Person> people) {
+        ArrayList incomeList = new ArrayList();
+        int medianIncome = 0;
+
+        for (Person person : people) {
+            incomeList.add(person.income);
+        }
+        Collections.sort(incomeList);
+
+        if (incomeList.size() % 2 == 0) {
+            int lowerMiddleIndex = (incomeList.size() / 2) - 1;
+            int higherMiddleIndex = (incomeList.size() / 2);
+            int lowerMiddleIncome = (int)incomeList.get(lowerMiddleIndex);
+            int higherMiddleIncome = (int)incomeList.get(higherMiddleIndex);
+            medianIncome = ((higherMiddleIncome - lowerMiddleIncome) / 2) + lowerMiddleIncome;
+            return medianIncome;
+        }
+        medianIncome = (int)incomeList.get((incomeList.size()) / 2);
+
+        return medianIncome;
+    }
+
+    public static List<String> alphabetizeByLastName(List<Person> people) {
+        ArrayList names = new ArrayList();
+        for (Person person : people) {
+            names.add(person.name);
+        }
+        Collections.sort(names);
+        return names;
+    }
 }
--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -42,4 +42,49 @@ public class UtilTest {
         Assert.assertEquals("College", mostCommon);
 
     }
+
+    @Test
+    public void calculateMedianIncomeShouldReturnMedianIncomeWhenEvenNumberOfPeople() {
+        Person person1 = new Person("Edgar McCormick", 57, "College", "$80,000");
+        Person person2 = new Person("Percy Hoffman", 39, "High School", "$55,000");
+        Person person3 = new Person("Tammy Ard", 27, "College", "$70,000");
+        Person person4 = new Person("Tom Hanks", 63, "College", "$60,000");
+
+        List<Person> people = Arrays.asList(person1, person2, person3, person4);
+
+        int expectedMedianIncome = Util.calculateMedianIncome(people);
+
+        Assert.assertEquals(65000, expectedMedianIncome);
+    }
+
+    @Test
+    public void calculateMedianIncomeShouldReturnMedianIncomeWhenOddNumberOfPeople() {
+        Person person1 = new Person("Edgar McCormick", 57, "College", "$80,000");
+        Person person2 = new Person("Percy Hoffman", 39, "High School", "$55,000");
+        Person person3 = new Person("Tammy Ard", 27, "College", "$70,000");
+
+        List<Person> people = Arrays.asList(person1, person2, person3);
+
+        int expectedMedianIncome = Util.calculateMedianIncome(people);
+
+        Assert.assertEquals(70000, expectedMedianIncome);
+    }
+
+    @Test
+    public void alphabetizeByLastNameShouldAlphabetizeNamesOrderedByLastName() {
+        Person person1 = new Person("Edgar McCormick", 57, "College", "$80,000");
+        Person person2 = new Person("Percy Hoffman", 39, "High School", "$55,000");
+        Person person3 = new Person("Tammy Ard", 27, "College", "$70,000");
+        Person person4 = new Person("Tammy Ark", 27, "College", "$70,000");
+
+        List<Person> people = Arrays.asList(person1, person2, person3, person4);
+
+        List<String> alphabetizedNames = Util.alphabetizeByLastName(people);
+
+
+        Assert.assertEquals("Tammy Ard", alphabetizedNames.get(0));
+        Assert.assertEquals("Tammy Ark", alphabetizedNames.get(1));
+        Assert.assertEquals("Percy Hoffman", alphabetizedNames.get(2));
+        Assert.assertEquals("Edgar McCormick", alphabetizedNames.get(3));
+    }
 }
